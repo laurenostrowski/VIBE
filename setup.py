2cc396535998e9caba3aec9645bfda02d1411b24
Lauren Ostrowski <113941356+laurenostrowski@users.noreply.github.com>
2026-06-16 11:58:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="vibe",
     version="0.1.0",
-    description="Vibrational Inversion to Biomechanical Estimates: "
-                "recover biomechanical vocal parameters from birdsong",
+    description="Vocal acoustic Inversion to Biomechanical Estimates: "
+                "recover biomechanical trajectories from birdsong",
     author="Lauren Ostrowski",
     license="MIT",
     packages=find_packages(),
     package_data={"vibe": ["a_b_sweep.pkl"]},
     include_package_data=True,
     install_requires=[
-        "numpy",
-        "scipy",
-        "torch",
-        "matplotlib",
-        "joblib",
-        "tqdm",
+        "numpy", "scipy", "torch", "matplotlib", "joblib", "tqdm",
+        'importlib_resources; python_version < "3.9"',
     ],
     python_requires=">=3.8",
 )
